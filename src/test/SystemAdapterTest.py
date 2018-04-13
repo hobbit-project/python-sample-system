ed5997341852e438dcb64f367b0a85cc3d6932f4
smirnp <smirnp@gmail.com>
2018-04-13 19:49:26 +0200

    Optmized for the DEBS_2018 benchmark

--- src/test/SystemAdapterTest.py
+++ src/test/SystemAdapterTest.py
@@ -9,18 +9,25 @@ from time import sleep
 from main.SystemAdapter import SystemAdapter
 from main.supplementary.HobbitCommandsListener import HobbitCommandsListener
 
-cwd = "/mnt/share/Projects/hobbit-java-sdk/target/"
-systemImageName = "dummybenchmark/system-adapter"
+# check/change it
+workingDir = "/mnt/share/Projects/DEBS-GC-2018/"
+jarFile = "target/debs_2018_gc_sample_system-1.0.jar"
+
+systemImageName = "git.project-hobbit.eu:4567/smirnp/sml-benchmark-v2/system-adapter"
+
+
+runningClass='org.hobbit.debs_2018_gc_samples.System.SampleSystemTestRunner'
+
 
 sessionId = "session_"+str(math.floor(time.time()))
 os.environ["HOBBIT_SESSION_ID"]=sessionId
 
-args = ['java -cp hobbit-java-sdk-1.1.5.jar org.hobbit.sdk.DummyBenchmarkTest2 '+ systemImageName+' '+ sessionId]
+args = ['java -cp '+jarFile+' '+runningClass+' '+ systemImageName+' '+ sessionId]
 
 
 def start_benchmark(arg):
     try:
-        process = subprocess.Popen(args, cwd=cwd, shell=True, stdout=subprocess.PIPE)
+        process = subprocess.Popen(args, cwd=workingDir, shell=True, stdout=subprocess.PIPE)
         pid = process.pid
         for line in iter(process.stdout.readline, ''):
             print(line)
